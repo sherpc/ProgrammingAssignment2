b9b9c174dc2e1ff6c7a68f373d77d46211cd708b
Aleksandr Sher <aleksandrsher@gmail.com>
2014-12-21 13:46:29 +0300

    Added comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,16 @@
+## Actually, I mean that this assigment implementation of memoized version of 
+## solve fn isn't good. We need to create some special cache matrix, when if R 
+## language possible to fast compare two matrices, we can write fn that will 
+## cache values without special cache matrix. And more, we can write general
+## "memoize" function, that will add cache functionality to any fn.
+
+## On other side, I'm understand, that assigment's version is more simple 
+## and more efficient in some cases. Also, if R can't compare two matrices 
+## fast, more general version will work slow on big inputs.
+
+
+## Assigment solution:
+
 ## makeCacheMatrix will create special memoized version of matrix.
 ## cacheSolve will solve memoized matrix, trying to get cache result if called 
 ## more than one time.
