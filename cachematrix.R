48d2455c593b6c40aacf8e0dae24a3561673b46c
Aleksandr Sher <aleksandrsher@gmail.com>
2014-12-21 13:39:46 +0300

    Added makeCacheMatrix and cacheSolve implementations

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,37 @@
 ## cacheSolve will solve memoized matrix, trying to get cache result if called 
 ## more than one time.
 
-## Write a short comment describing this function
+## Returns special memoized version of matrix. 
+## Needed for cacheSolve function (see below).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	solution <- NULL
+	set <- function(y) {
+		x <<- y
+		solution <<- NULL
+	}
+	get <- function() x
+	setsolution <- function(mean) solution <<- mean
+	getsolution <- function() solution
+	list(set = set, 
+		 get = get,
+		 setsolution = setsolution,
+		 getsolution = getsolution)
 }
 
 
-## Write a short comment describing this function
+## Solve special memoized version of matrix. 
+## Function trying to extract cache value, and only if cache value is NULL it
+## will actually solve matrix (and store result in cache).
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+	solution <- x$getsolution()
+	if(!is.null(solution)) {
+		message("getting cached data")
+		return(solution)
+	}
+	data <- x$get()
+	solution <- solve(data)
+	x$setsolution(solution)
+	solution
 }
